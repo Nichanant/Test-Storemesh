e6094dc644b276e36b2766dc3e22da6bbcc8b4bb
Nichanant <nichanant.work@gmail.com>
2025-01-20 01:46:53 +0700

    fix-product

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -6,11 +6,11 @@ function ListItem(props) {
   const { id, title, price, src } = props
 
   return (
-    <div className='container flex justify-between bg-white py-5 my-2'>
+    <div className='flex justify-between bg-white py-5 my-2 w-full'>
       <div className='container flex flex-col items-start justify-center'>
         <p className='mx-10 text-2xl'> <span className='bg-blue-200 px-0.5 rounded-md'>ID :</span> {id}</p>
-        <p className='mx-10 text-lg'><span  className='bg-blue-200 px-0.5 rounded-md'>Product : </span>{title}</p>
-        <p className='mx-10 text-lg'><span  className='bg-blue-200 px-0.5 rounded-md'>Price : </span>${price}</p>
+        <p className='mx-10 text-lg'><span  className='bg-blue-200 px-0.5 rounded-md'>Product :</span> {title}</p>
+        <p className='mx-10 text-lg'><span  className='bg-blue-200 px-0.5 rounded-md'>Price :</span> ${price}</p>
       </div>
       <img src={src} alt="pic Product" width={200} className='px-10'/>
     </div>
--- src/components/ShowItem.jsx
+++ src/components/ShowItem.jsx
@@ -34,7 +34,7 @@ function ShowItem() {
 
         <div className='bg-gray-200 w-screen items-start'>
 
-            <div className='container flex justify-between'>
+            <div className='flex justify-between'>
                 <h1 className='text-4xl m-5 text-blue-800'>Products</h1>
                 <div className='m-5'> Sort By :
                     <select className='bg-white w-32 h-10 rounded-md self-center mx-6 px-2'>
@@ -54,15 +54,15 @@ function ShowItem() {
 
            
 
-            <div className='pagination container flex justify-end bg-blue-400 p-3'>
-                <ul className='flex space-x-2'>
-                    <li><a href="#" className='bg-blue-100 px-3 py-1 rounded-md'>{`<`}</a></li>
-                    <li><a href="#" className='bg-blue-100 px-3 py-1 rounded-md'>1</a></li>
-                    <li><a href="#" className='bg-blue-100 px-3 py-1 rounded-md'>2</a></li>
-                    <li><a href="#" className='bg-blue-100 px-3 py-1 rounded-md'>3</a></li>
-                    <li><a href="#" className='bg-blue-100 px-3 py-1 rounded-md'>4</a></li>
-                    <li><a href="#" className='bg-blue-100 px-3 py-1 rounded-md'>5</a></li>
-                    <li><a href="#" className='bg-blue-100 px-3 py-1 rounded-md'>{`>`}</a></li>
+            <div className='pagination flex justify-end bg-blue-400 p-3'>
+                <ul className='flex space-x-2 mx-5'>
+                    <li><a href="#" className='bg-blue-100 px-3 py-1 rounded-md hover:bg-blue-800 hover:text-white'>{`<`}</a></li>
+                    <li><a href="#" className='bg-blue-100 px-3 py-1 rounded-md hover:bg-blue-800 hover:text-white'>1</a></li>
+                    <li><a href="#" className='bg-blue-100 px-3 py-1 rounded-md hover:bg-blue-800 hover:text-white'>2</a></li>
+                    <li><a href="#" className='bg-blue-100 px-3 py-1 rounded-md hover:bg-blue-800 hover:text-white'>3</a></li>
+                    <li><a href="#" className='bg-blue-100 px-3 py-1 rounded-md hover:bg-blue-800 hover:text-white'>4</a></li>
+                    <li><a href="#" className='bg-blue-100 px-3 py-1 rounded-md hover:bg-blue-800 hover:text-white'>5</a></li>
+                    <li><a href="#" className='bg-blue-100 px-3 py-1 rounded-md hover:bg-blue-800 hover:text-white'>{`>`}</a></li>
                 </ul>
             </div>
 
